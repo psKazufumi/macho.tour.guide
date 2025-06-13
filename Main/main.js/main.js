ddda69e6f3e1cdb9d478aa1ec1206b5222ac09d0
psKazufumi <95842650+psKazufumi@users.noreply.github.com>
2025-06-12 21:28:30 -0600

    Add files via upload

--- Main/main.js/main.js
+++ Main/main.js/main.js
@@ -1,2 +1,18 @@
 scroll({ reset:true });
-ScrollReveal().reveal(".header", { delay:500 })
+ScrollReveal().reveal(".header", { delay:500 });
+
+
+// 交差すると色が変わる
+const menu = document.getElementById('menu');
+const foto = document.getElementById('foto');
+const n = document.querySelector('e_aboutus');
+
+const options = {
+    threshold: 1
+};
+
+const observer = new IntersectionObserver(showElement, options);
+observer.observe(n);
+function showElement(){
+    n.style.opacity = 0;
+};
